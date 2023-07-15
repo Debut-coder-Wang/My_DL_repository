5d08f678f896c24ce27eeba15456d564cc860d9b
Debut-coder-Wang <94164666+Debut-coder-Wang@users.noreply.github.com>
2023-07-15 13:42:42 +0800

    Update DataCreation.py

--- Data_Creation/DataCreation.py
+++ Data_Creation/DataCreation.py
@@ -23,25 +23,6 @@ class MyData(Dataset):
             img = self.transform(img)
 
         return (img, label)
-# class MyData(Dataset):
-#     def __init__(self, csv_file, root_dir, transform=None):
-#         self.annotations = pd.read_csv(csv_file)
-#         self.root_dir = root_dir
-#         self.transform = transform
-#
-#     def __len__(self):
-#         return len(self.annotations)
-#
-#     def __getitem__(self, index):
-#         img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
-#         image = Image.open(img_path)
-#         y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
-#
-#         if self.transform:
-#             image = self.transform(image)
-#
-#         return (image, y_label)
-
 
 '''
 1、这里注意datasets和Dataset,前者是一个方法属于torchvision，后者是一个类属于torch  
